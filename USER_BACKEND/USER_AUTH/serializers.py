6a664b79323995aa3838c0b708572f3063d14723
Jatin Singhal <125295355+PSYCHNERD2512@users.noreply.github.com>
2023-02-28 16:30:32 +0530

    Update serializers.py

--- USER_BACKEND/USER_AUTH/serializers.py
+++ USER_BACKEND/USER_AUTH/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
 
 
 class UserSerializer(serializers.ModelSerializer):
+    
+    def validate_password(self, value: str) -> str:
+    
+        return make_password(value)
 
     class Meta:
         model = get_user_model()
