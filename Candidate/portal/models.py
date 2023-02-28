ffec431bf0340881778e1fdf4d9e70836aaf42a0
Shivam Agarwal <Agarwalshivam805@gmail.com>
2023-02-28 15:41:30 +0530

    API left

--- Candidate/portal/models.py
+++ Candidate/portal/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.http import HttpResponse
 
 class Candidate(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     email=models.EmailField()
     image=models.ImageField()
-    age=models.IntegerField()
+    age=models.IntegerField(max_digits=2)
     votes=models.IntegerField()
-    
+
     def __str__(self):
         return self.user.first_name
