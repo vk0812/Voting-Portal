6903129c8abd7deec7cb488ad73577650f002856
Shivam Agarwal <Agarwalshivam805@gmail.com>
2023-03-02 01:32:55 +0530

    done

--- Candidate/portal/models.py
+++ Candidate/portal/models.py
@@ -3,10 +3,10 @@ from django.contrib.auth.models import User
 
 
 class Candidate(models.Model):
-    user= models.CharField(User, max_length=100)
+    user= models.CharField(verbose_name='Candidate', max_length=100)
     email=models.EmailField(verbose_name='email')
-    image=models.ImageField(verbose_name='Profile Picture')
     votes=models.IntegerField(default=0, verbose_name='votes')
+    id=models.IntegerField(primary_key=True)
 
     def __str__(self):
         return self.user
--- Candidate/portal/views.py
+++ Candidate/portal/views.py
@@ -4,6 +4,7 @@ from rest_framework.decorators import api_view #,permission_classes
 from portal.models import Candidate
 from portal.serializers import CandidateSerializer
 from rest_framework import serializers
+from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 
 
@@ -11,22 +12,33 @@ def CastVote(votes):
     votes+=1
 
 @api_view(['GET'])
-# @permission_classes((permissions.AllowAny,))
+
 def info(request):
     candidate=Candidate.objects.all()
     serializer =CandidateSerializer(candidate, many=True)
     return Response(serializer.data)
 
-@api_view(['POST'])
-# @permission_classes((permissions.AllowAny,))
+@api_view(['PUT'])
+
 def Vote(request):
-    if CastVote(request.data['votes']):
-        request 
-    vote=Candidate.objects.filter('votes')
-    serializer=CandidateSerializer(vote)
-    if serializer.is_valid() :
-        serializer.save
+    data=JSONParser().parse(request)
+    candidate_data=Candidate.objects.get(id=data['id'])
+    serializer=CandidateSerializer(candidate_data)
+    dict= serializer.data
+    dict["votes"]= dict["votes"]+1
+    serializer=CandidateSerializer(candidate_data,data=dict)
+    if serializer.is_valid():
+        serializer.save()
+        print(serializer.data)
+    else:
+        print(serializer.errors)
     return Response(serializer.data)
+    # if CastVote(request.data['id']):
+    #     request 
+    # vote=Candidate.objects.filter('votes')
+    # serializer=CandidateSerializer(vote)
+    
+    # return Response(serializer.data)
 
 
 
